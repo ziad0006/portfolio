e19c39f1d336f358988905be2eea90502097a1e5
ziad0006 <azyad5700@gmail.com>
2025-01-09 05:44:03 +0200

    Update Notfound.jsx

--- src/component/Notfound/Notfound.jsx
+++ src/component/Notfound/Notfound.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Notfound() {
   return (
-    <div>
-      <h1>Notfound</h1>
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className=' bg-red-500 p-5 my-7 text-5xl'> This Page Notfound</h1>
     </div>
   )
 }
