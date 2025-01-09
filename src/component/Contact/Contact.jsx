a890191d1c6a9b24ca329b43b97836e3fb527903
ziad0006 <azyad5700@gmail.com>
2025-01-09 05:41:48 +0200

    Update Contact.jsx

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -4,9 +4,7 @@ export default function Contact() {
     return (
         <>
             <div className='contact mt-36 h-screen'>
-                <h2 className='text-5xl  text-[#2C3E50] font-bold '>CONTACT SECTION</h2>
-
-
+                <h2 className='text-4xl  md:text-5xl  text-[#2C3E50] font-bold '>CONTACT SECTION</h2>
 
                 <div className=" flex text-[#2C3E50]  justify-center items-center mt-5">
                     <div className="line "></div>
@@ -17,7 +15,7 @@ export default function Contact() {
 
 
                 <div className="lg:w-[50%] mx-3 lg:mx-auto relative  mt-[4rem] ">
-                 
+
                     <div className="relative z-0 w-full mb-7 group">
                         <input type="text" name="username" id="name" className=" block py-7  w-full text-sm text-gray-900 bg-transparent border-b-2 rounded-sm appearance-none focus:outline-none focus:ring-0 peer placeholder:text-lg placeholder:text-gray-700 " placeholder=" userName" required />
                     </div>
@@ -30,7 +28,7 @@ export default function Contact() {
                     <div className="relative z-0 w-full mb-7 group">
                         <input type="text" name="username" id="name" className="block py-7 w-full text-sm text-gray-900 bg-transparent rounded-sm border-b-2  appearance-none focus:outline-none focus:ring-0 peer placeholder:text-lg placeholder:text-gray-700 " placeholder=" userPassword" required />
                     </div>
-                   
+
                     <button type="submit" className="absolute left-0  text-white bg-[#1ABC9C] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center ">Send Massage</button>
                 </div>
 
@@ -38,3 +36,4 @@ export default function Contact() {
         </>
     )
 }
+
